673741070d4c2bf39001e742a55852a0a4a80299
EGSPrompt <egs@eminencegrowthsolutions.com>
2025-03-29 19:40:39 -0400

    Fix: removed reportWebVitals and updated ESLint config

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,6 +10,4 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
-
-reportWebVitals(); 
+); 
