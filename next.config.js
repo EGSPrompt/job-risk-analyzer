407c4097015cf3e4b8be3c57795476fc97c660a3
EGSPrompt <egs@eminencegrowthsolutions.com>
2025-03-29 21:11:26 -0400

    Fix: remove export mode from Next config to restore routing

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: '/risk-analyzer',
-  async rewrites() {
-    return [
-      {
-        source: '/risk-analyzer',
-        destination: '/',
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig; 
